15331f11ff319a392ac5f0f2d7f4c8598042ba96
fakorede <abiolafakorede@gmail.com>
2018-12-08 20:37:13 +0100

    view created to mark todo as complete

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -25,6 +25,15 @@
 
         {{ $todo->todo }}   <a href="{{ route('todo.delete', ['id' => $todo->id]) }}" class="btn btn-danger btn-md">Delete Todo</a>
         <a href="{{ route('todo.update', ['id' => $todo->id]) }}" class="btn btn-info btn-md">Update Todo</a>
+        @if(!$todo->completed)
+
+            <a href="{{ route('todos.completed', [ 'id' => $todo->id ])}}" class="btn btn-success btn-md">Mark as completed</a>
+
+        @else
+
+            <span class="text-success">Completed!</span>
+
+        @endif
         <hr>
 
     @endforeach
